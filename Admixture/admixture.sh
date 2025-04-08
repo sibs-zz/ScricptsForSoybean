2cc5d9ca7ff5f756b11e4d422578071ea5a51bdc
data <55496125+sibs-zz@users.noreply.github.com>
2025-04-08 16:54:04 +0800

    Update and rename admixture to admixture.sh

--- Admixture/admixture.sh
+++ Admixture/admixture.sh
@@ -4,6 +4,6 @@ cpulimit -l 400 -- admixture --cv 8917.snp.filter.bed 5 -j4 >> log.txt
 cpulimit -l 400 -- admixture --cv 8917.snp.filter.bed 6 -j4 >> log.txt
 cpulimit -l 400 -- admixture --cv 8917.snp.filter.bed 7 -j4 >> log.txt
 # load library for use in R
-library(pophelperShiny)
+#library(pophelperShiny)
 # launch app
-runPophelper()
+#runPophelper()
