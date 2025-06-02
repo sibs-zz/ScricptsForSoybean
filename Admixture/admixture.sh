590059c49d2652a186014adda0938e40f76e9062
data <55496125+sibs-zz@users.noreply.github.com>
2025-06-02 14:33:16 +0800

    Update admixture.sh

--- Admixture/admixture.sh
+++ Admixture/admixture.sh
@@ -3,7 +3,7 @@ cpulimit -l 400 -- admixture --cv 8917.snp.filter.bed 4 -j4 >> log.txt
 cpulimit -l 400 -- admixture --cv 8917.snp.filter.bed 5 -j4 >> log.txt
 cpulimit -l 400 -- admixture --cv 8917.snp.filter.bed 6 -j4 >> log.txt
 cpulimit -l 400 -- admixture --cv 8917.snp.filter.bed 7 -j4 >> log.txt
-# load library for use in R
+#load library for use in R
 #library(pophelperShiny)
-# launch app
+#launch app
 #runPophelper()
