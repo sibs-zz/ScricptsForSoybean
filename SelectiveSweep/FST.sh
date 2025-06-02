605834fc18effe8c33213c6b3f47c02ff9ea9bf8
data <55496125+sibs-zz@users.noreply.github.com>
2025-06-02 14:30:55 +0800

    Update FST.sh

--- SelectiveSweep/FST.sh
+++ SelectiveSweep/FST.sh
@@ -3,7 +3,7 @@ nohup vcftools --vcf 8917.snp.filter.maf0.01.int0.7.clean.beagle.vcf --weir-fst-
 
 grep -v BIN zhongguoallvsbaxi.windowed.weir.fst|awk '{print $1,$2,$3,$5}' > zhongguoallvsbaxi.windowed.weir.fst.t
 python zscore.py zhongguoallvsbaxi.windowed.weir.fst.t zhongguoallvsbaxi.windowed.weir.fst.t.zscore
-awk '{if($5>=1.645) print $1,$2,$3}' zhongguoallvsbaxi.windowed.weir.fst.t.zscore  > zhongguoallvsbaxi.windowed.weir.fst.t.zscore.pos
+awk '{if($5>=1.96) print $1,$2,$3}' zhongguoallvsbaxi.windowed.weir.fst.t.zscore  > zhongguoallvsbaxi.windowed.weir.fst.t.zscore.pos
 awk '{if($1<10){print "Chr0"$1,$2,$3}else {print "Chr"$1,$2,$3}}' zhongguoallvsbaxi.windowed.weir.fst.t.zscore.pos > zhongguoallvsbaxi.windowed.weir.fst.t.zscore.pos.t
 python GetGeneFromGFF.py ZH13.gene.gff zhongguoallvsbaxi.windowed.weir.fst.t.zscore.pos.t zhongguoallvsbaxi.windowed.weir.fst.t.zscore.pos.t.gene
 awk '{print $9}' zhongguoallvsbaxi.windowed.weir.fst.t.zscore.pos.t.gene |grep -v attr|sed s/ID=//g|sed s/\;//g |sort -u > zhongguoallvsbaxi.windowed.weir.fst.t.zscore.pos.t.gene.clean
